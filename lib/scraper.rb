94004d429850ba7d468639d6a615da63bc43b96b
Alex Wilson <fonzmp@gmail.com>
2018-05-22 02:57:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,8 @@ class Scraper
   Scraper.new.get_page
 
   def get_courses
-    html.css(".post").css("h2")
+    html.each do |course|
+      course.css(".post").css("h2")
   end
 
   def make_courses
