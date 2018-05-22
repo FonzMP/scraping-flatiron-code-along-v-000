5e83a43503fda0d56789bc54f1eca576e7306353
Alex Wilson <fonzmp@gmail.com>
2018-05-22 02:43:38 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -3,7 +3,9 @@ class Course
   attr_accessor :title, :schedule, :description
 
   def initialize(title, schedule, description)
-
+    @title = title
+    @schedule = schedule
+    @description = description
   end
 
 end
