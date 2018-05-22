bfd12a64e1124b14f3a1e507bd6422b69e447a0e
Alex Wilson <fonzmp@gmail.com>
2018-05-22 02:44:45 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -4,15 +4,15 @@ class Course
 
   @@all = []
 
-  def initialize(title, schedule, description)
-    @title = title
-    @schedule = schedule
-    @description = description
-    @@all << self
-  end
-
-  def all
-    @@all
-  end
+  # def initialize(title, schedule, description)
+  #   @title = title
+  #   @schedule = schedule
+  #   @description = description
+  #   @@all << self
+  # end
+  #
+  # def all
+  #   @@all
+  # end
 
 end
